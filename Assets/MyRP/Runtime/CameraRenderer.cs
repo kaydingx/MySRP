5708e58bf23d76794659175d380ebc66a4b4df98
kaydingx <kayding@yeah.net>
2022-10-12 23:10:47 +0800

    add command buffer

--- Assets/MyRP/Runtime/CameraRenderer.cs
+++ Assets/MyRP/Runtime/CameraRenderer.cs
@@ -8,15 +8,32 @@ public class CameraRenderer
     private ScriptableRenderContext context;
     private Camera camera;
 
+    private const string bufferName = "My Camera Render";
+
+    private CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
 
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
         
+        Setup();
         
         DrawVisibleGeometry();
         Submit();
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        buffer.BeginSample(bufferName);
+        ExecuteBuffer();
     }
 
 
@@ -27,6 +44,14 @@ public class CameraRenderer
 
     void Submit()
     {
+        buffer.EndSample(bufferName);
+        ExecuteBuffer();
         context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
     }
 }
